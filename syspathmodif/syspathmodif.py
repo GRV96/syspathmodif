da499952f0c68b38b6a1fb88d9a2cac1ceef37dc
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-09-20 15:19:41 -0400

    The three public functions have a docstring.

--- syspathmodif/syspathmodif.py
+++ syspathmodif/syspathmodif.py
@@ -6,6 +6,17 @@ _SYS_PATH = sys.path
 
 
 def sp_append(some_path):
+	"""
+	Appends the given path to the end of list sys.path if it does not already
+	contain the path. If the path is of type pathlib.Path, it will be converted
+	to a string.
+
+	Args:
+		some_path (str or pathlib.Path): the path to append to sys.path.
+
+	Throws:
+		TypeError: if argument some_path is not of type str or pathlib.Path.
+	"""
 	some_path = _ensure_path_is_str(some_path)
 
 	if some_path not in _SYS_PATH:
@@ -13,11 +24,32 @@ def sp_append(some_path):
 
 
 def sp_contains(some_path):
+	"""
+	Verifies whether list sys.path contains the given path.
+
+	Args:
+		some_path (str or pathlib.Path): the path whose presence is verified.
+
+	Returns:
+		bool: True if sys.path contains argument some_path, False otherwise.
+
+	Throws:
+		TypeError: if argument some_path is not of type str or pathlib.Path.
+	"""
 	some_path = _ensure_path_is_str(some_path)
 	return some_path in _SYS_PATH
 
 
 def sp_remove(some_path):
+	"""
+	Removes the given path from list sys.path if it contains the path.
+
+	Args:
+		some_path (str or pathlib.Path): the path to remove from sys.path.
+
+	Throws:
+		TypeError: if argument some_path is not of type str or pathlib.Path.
+	"""
 	some_path = _ensure_path_is_str(some_path)
 
 	if some_path in _SYS_PATH:
