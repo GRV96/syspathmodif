2f0bcf3fcee7b9f3157f405b2db883ed27bf60d4
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-09 12:38:49 -0500

    Function `sp_remove`'s docstring mentions None arguments.

--- syspathmodif/syspathmodif.py
+++ syspathmodif/syspathmodif.py
@@ -52,7 +52,8 @@ def sp_contains(some_path):
 
 def sp_remove(some_path):
 	"""
-	Removes the given path from list sys.path if it contains the path.
+	Removes the given path from list sys.path if it contains the path. If the
+	path is None, this method does not change sys.path.
 
 	Args:
 		some_path (str or pathlib.Path): the path to remove from sys.path.
