a083fb0963323e105a5a49257f4fd0bdfa2c56c0
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-02 00:29:51 -0500

    The `__repr__` methods in `demo_package` use a formatted string.

--- demo_package/ajxo.py
+++ demo_package/ajxo.py
@@ -7,7 +7,7 @@ class Ajxo:
 
 	def __repr__(self):
 		return self.__class__.__name__\
-			+ "(" + repr(self.a) + ", " + repr(self.b) + ")"
+			+ f"({repr(self.a)}, {repr(self.b)})"
 
 
 __all__ = [Ajxo.__name__]
--- demo_package/point.py
+++ demo_package/point.py
@@ -6,7 +6,7 @@ class Point:
 
 	def __repr__(self):
 		return self.__class__.__name__\
-			+ "(" + str(self._x) + ", " + str(self._y) + ")"
+			+ f"({self._x}, {self._y})"
 
 	@property
 	def x(self):
