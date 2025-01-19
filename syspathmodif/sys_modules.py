82a1d61c06ea5c5d0b6f6689ed2703817ff972a7
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-19 17:17:32 -0500

    Docstring of `sm_contains` more informative

--- syspathmodif/sys_modules.py
+++ syspathmodif/sys_modules.py
@@ -3,14 +3,15 @@ import sys
 
 def sm_contains(module_name):
 	"""
-	Indicates whether dictionary sys.modules contains the module or package
-	whose name is the argument.
+	Dictionary sys.modules maps module and package names (str) to the
+	corresponding module or package. This function indicates whether
+	sys.modules contains the module or package whose name is the argument.
 
 	Args:
 		module_name (str): the name of a module or package.
 
 	Returns:
-		bool: True if sys.modules contains argument module_name, False
+		bool: True if argument module_name is a key in sys.modules, False
 			otherwise.
 	"""
 	return module_name in sys.modules
