eb93ef8b2845b5acd54fcf4f78e7af1e0e498fec
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-20 11:00:54 -0500

    Version incremented to `1.4.0`

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
 	setuptools.setup(
 		name = "syspathmodif",
-		version = "1.3.1",
+		version = "1.4.0",
 		author = "Guyllaume Rousseau",
 		description = short_desc,
 		long_description = long_desc,
