657824f0299205bed2e450f01651571640dbc52e
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-09-16 10:43:06 -0400

    [22] `SysPathBundle.__enter__` return type: `Self`.

--- syspathmodif/syspathbundle.py
+++ syspathmodif/syspathbundle.py
@@ -1,7 +1,7 @@
 # __all__ declared at the module's end
 
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Self
 
 from strath import ensure_path_is_str
 
@@ -59,7 +59,7 @@ class SysPathBundle:
 		if self._cleared_on_del:
 			self.clear()
 
-	def __enter__(self):
+	def __enter__(self) -> Self:
 		return self
 
 	def __exit__(self, exc_type, exc_value, traceback) -> None:
