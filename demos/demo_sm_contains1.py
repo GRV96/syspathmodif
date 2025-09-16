f0731db5a0bdc751b07760dd3d7ea6ca8e2ca2ae
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-09-16 15:33:30 -0400

    Better console messages in first `sm_contains` demo.

--- demos/demo_sm_contains1.py
+++ demos/demo_sm_contains1.py
@@ -31,13 +31,13 @@ _add_demo_package_to_sys_modules()
 
 # To import Ajxo
 if not sm_contains("demo_package"):
-	print("The repository's root will be added to sys.path")
 	paths.append(REPO_ROOT)
+	print("Repository's root added to sys.path.")
 
 # To import Point
 if not sm_contains("point"):
-	print("Directory demo_package will be added to sys.path")
 	paths.append(REPO_ROOT/"demo_package")
+	print("Directory demo_package added to sys.path.")
 
 with SysPathBundle(paths):
 	from demo_package import Ajxo
