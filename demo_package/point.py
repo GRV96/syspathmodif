f1b820ffba73e7d28151fac30d4f98913d8f1810
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-09-16 11:05:01 -0400

    [22] Type hints in `demo_package`

--- demo_package/point.py
+++ demo_package/point.py
@@ -1,19 +1,19 @@
 class Point:
 
-	def __init__(self, x, y):
+	def __init__(self, x: int, y: int) -> None:
 		self._x = x
 		self._y = y
 
-	def __repr__(self):
+	def __repr__(self) -> str:
 		return self.__class__.__name__\
 			+ f"({self._x}, {self._y})"
 
 	@property
-	def x(self):
+	def x(self) -> int:
 		return self._x
 
 	@property
-	def y(self):
+	def y(self) -> int:
 		return self._y
 
 
