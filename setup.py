e8ec56b191cfc259b285b3f134b018a98f08ea8b
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-09-29 21:26:00 -0400

    [34] Python version `3.10` in `setup.py`.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 			"Intended Audience :: Developers",
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
-			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.10",
 			"Topic :: Software Development :: Libraries :: Python Modules",
 			"Topic :: Utilities"
 		],
